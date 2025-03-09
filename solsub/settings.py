86ad4ffdddd60c397f07874268ab309cf4b685ff
Veda-rathna <vedarathna057@gmail.com>
2025-03-09 14:55:21 +0530

    changed ALLOWED_HOSTS

--- solsub/settings.py
+++ solsub/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-7-iamrm%2rhf4elz7&j%irgh!sz8$&j^yj%mgy%d5y_x5e2f6+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['solsub.onrender.com']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
